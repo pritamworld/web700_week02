a3abe49481ab1e57f8833239ab56d03dd6ccce7c
moxDroid Labs Inc <pritam.world@gmail.com>
2023-05-17 16:30:30 -0400

    function all added

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ console.hello = function () {
 }
 console.log(console)
 console.hello()
+
+//Passing function as param
+
+var add = function(a, b){
+    return a+b
+}
+
+function artihmatic(fn, a , b){
+    return fn(a,b)
+}
+
+var r = artihmatic(add, 10 ,20)
+console.log(r)
+
+//Inline declaration
+var r = artihmatic(function(a, b) {
+    return a - b
+}, 10 ,20)
+console.log(r)
